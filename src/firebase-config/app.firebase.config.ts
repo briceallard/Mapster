bc7b7bd3494714363e724cde1ffeda87535612f9
Brice Allard <briceallard@gmail.com>
2018-11-20 00:32:51 -0600

    restructuring camera folders

--- src/firebase-config/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyC8eOIjnb_seE8YheOGt__GBWt4Wk0Qd24",
-    authDomain: "mapster-ef2ae.firebaseapp.com",
-    databaseURL: "https://mapster-ef2ae.firebaseio.com",
-    projectId: "mapster-ef2ae",
-    storageBucket: "mapster-ef2ae.appspot.com",
-    messagingSenderId: "255493036476"
-  }
