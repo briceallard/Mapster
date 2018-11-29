4f757d06f27f62bbafaa31b4eac1550818d6ecaf
Brice Allard <briceallard@gmail.com>
2018-11-29 07:37:16 -0600

    Search for friends added

--- src/pages/friend-search/friend-search.ts
+++ src/pages/friend-search/friend-search.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AuthProvider } from '../../providers/auth/auth';
+import { User } from '../../models/users/user.interface';
+import { Observable } from 'rxjs';
+import { UtilitiesProvider } from '../../providers/utilities/utilities';
 
 /**
  * Generated class for the FriendSearchPage page.
@@ -15,11 +20,51 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FriendSearchPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public users: Observable<User[]>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private data: AngularFirestore,
+    private auth: AuthProvider,
+    private alertControl: UtilitiesProvider
+  ) {
+
+  }
+
+  ionViewWillLoad() {
+    this.updateUserList();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FriendSearchPage');
+  }
+
+  updateUserList() {
+    this.users = this.data.collection<User>('users').valueChanges();
+  }
+
+  queryExample(): Observable<any> {
+    let retardedUsers = this.data.collection<User>('users', ref => 
+      ref.where("retarded", "==", true)).valueChanges();
+
+    return this.data.collection<any>('users', ref => 
+      ref.where("retarded", "==", true)
+          .orderBy("lastName", "desc")
+          .limit(3)
+          
+      //  .orderBy("lastName")
+        // etc etc
+    ).valueChanges();              
+
+    // Returns and observable<T> return this.data.collection<any>
+  }
+
+  
+
+  confirmFriendRequest() {
+    var title: 'Confirm';
+    var message: 'Add this user as a friend?';
+    this.alertControl.confirmAlert(title, message,(console.log('Request')));
+    // SEND REQUEST MESSAGE AS CALLBACK ABOVE    
   }
 
 }
